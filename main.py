7093372894cd8db9adc28d66b18b3a251df39955
shimon <shimon.on5@gmail.com>
2021-08-02 16:47:07 +0300

    working safe version. control tension and angle in middlecheck

--- main.py
+++ main.py
@@ -41,75 +41,55 @@ class SnakeExp:
         self.final_angles_state = []
         self.final_strings_state = []
         self.command = []
+        self.file = open('output.txt', 'a')
+        self.file.write("initial_angles_state$initial_strings_state$command$final_angles_state$final_strings_state\n")
+
 
     def write_line(self, file):
         file.write(
-            self.initial_angles_state.__str__() + '$' + self.initial_strings_state.__str__() + '$' + self.command.__str__() + '$' + self.final_angles_state.__str__() + '$' + self.final_strings_state.__str__())
-
-    def get_current_state(self):
-        pass
+            self.initial_angles_state.__str__() + '$' + self.initial_strings_state.__str__() + '$' + self.command.data.__str__() + '$' + self.final_angles_state.__str__() + '$' + self.final_strings_state.__str__())
+        file.write("\n")
 
     def run_experiment(self):
-        file = open('output.txt', 'a')
         while not rospy.is_shutdown():
             for i in range(100):
-                self.initial_angles_state = self.current_angles_state
-                self.initial_strings_state = self.current_strings_state
+                # self.initial_angles_state = self.current_angles_state
+                # self.initial_strings_state = self.current_strings_state
+                self.middlecheck()
                 self.command = random.choice(possible_actions)
+                self.command = Int32MultiArray(data=np.asarray(self.command).astype(int))
                 self.send_motor_cmd()
-                self.final_angles_state = self.current_angles_state
-                self.final_strings_state = self.current_strings_state
-                self.write_line(file)
+                # self.final_angles_state = self.current_angles_state
+                # self.final_strings_state = self.current_strings_state
+                # self.write_line(file)
                 time.sleep(0.2)
-
             self.stop_motor()
-        file.close()
+        self.file.close()
 
     def joint_val_update(self, msg):
-        print("msg_data: ", msg.data)
-        for i in range(len(msg.data)-1):
-            if msg.data[i] > max_angl:
-                rospy.logfatal("you reach max angle")
-                # self.reduce_tension(2*i)
-                # self.add_tension(2*i+1)
-                self.stop_motor()
-                break
-
-            if msg.data[i] < min_angl:
-                rospy.logfatal("you reach min angle")
-                # self.add_tension(2*i)
-                # self.reduce_tension(2*i+1)
-                self.stop_motor()
-                break
+        # print("msg_data: ", msg.data)
         self.current_angles_state = msg.data
-        print("current_joint: ", self.current_angles_state)
+        # print("current_joint: ", self.current_angles_state)
 
     def tension_val_update(self, msg):
-        print(msg)
-        for i in range(len(msg.data)-1):
-            if msg.data[i] > max_tension:
-                rospy.logfatal("you reach max tension")
-                self.reduce_tension(i)
-                break
-
-            if msg.data[i] < min_tension:
-                rospy.logfatal("you reach min tension")
-                self.add_tension(i)
-                break
-
+        # print(msg)
         self.current_strings_state = msg.data
 
-    def send_motor_cmd(self):
-        msg = Int32MultiArray(data=np.asarray(self.command).astype(int))
-        self.motor_pwm_pub.publish(msg)
-        time.sleep(delay)
+    def send_motor_cmd(self, d=delay):
+        self.initial_angles_state = self.current_angles_state
+        self.initial_strings_state = self.current_strings_state
+        self.motor_pwm_pub.publish(self.command)
+        time.sleep(d)
         self.pub_zero()
+        self.final_angles_state = self.current_angles_state
+        self.final_strings_state = self.current_strings_state
+        self.write_line(self.file)
 
     def stop_motor(self):
         node_process = Popen(
-            shlex.split('rosrun robot_snake_4 controller_v4_3')
+            shlex.split('rosrun robot_snake_4 controller_v4_3'), shell=False
         )
-        time.sleep(10)
+        time.sleep(8)
 
         # stop
         node_process.terminate()
@@ -119,19 +99,55 @@ class SnakeExp:
         self.motor_pwm_pub.publish(msg)
 
     def add_tension(self, ind):
-        msg = Int32MultiArray(data=np.zeros(_N_string).astype(int))
-        msg.data[ind] = motor_vel_step_up
-        self.motor_pwm_pub.publish(msg)
-        time.sleep(delay)
-        self.pub_zero()
+        while self.current_strings_state[ind] < min_tension:
+            msg = Int32MultiArray(data=np.zeros(_N_string).astype(int))
+            msg.data[ind] = motor_vel_step_up
+            self.command = msg
+            self.send_motor_cmd()
 
     def reduce_tension(self, ind):
+        while self.current_strings_state[ind] > max_tension:
+            msg = Int32MultiArray(data=np.zeros(_N_string).astype(int))
+            msg.data[ind] = motor_vel_step_down
+            self.command = msg
+            self.send_motor_cmd()
+
+    def add_angle(self, ind):
+        msg = Int32MultiArray(data=np.zeros(_N_string).astype(int))
+        msg.data[ind] = motor_vel_step_up
+        msg.data[ind+1] = motor_vel_step_down
+        self.command = msg
+        self.send_motor_cmd()
+
+    def reduce_angle(self, ind):
         msg = Int32MultiArray(data=np.zeros(_N_string).astype(int))
         msg.data[ind] = motor_vel_step_down
-        self.motor_pwm_pub.publish(msg)
-        time.sleep(delay)
-        self.pub_zero()
+        msg.data[ind+1] = motor_vel_step_up
+        self.command = msg
+        self.send_motor_cmd(0.1)
 
+    def middlecheck(self):
+        for i in range(len(self.current_strings_state)-1):
+            if self.current_strings_state[i] > max_tension:
+                rospy.logfatal("you reach max tension")
+                self.reduce_tension(i)
+
+            if self.current_strings_state[i] < min_tension:
+                rospy.logfatal("you reach min tension")
+                self.add_tension(i)
+
+        for i in range(len(self.current_angles_state)-1):
+            if self.current_angles_state[i] > max_angl:
+                rospy.logfatal("you reach max angle")
+                # self.reduce_tension(2*i)
+                # self.add_tension(2*i+1)
+                self.reduce_angle(i)
+
+            if self.current_angles_state[i] < min_angl:
+                rospy.logfatal("you reach min angle")
+                # self.add_tension(2*i)
+                # self.reduce_tension(2*i+1)
+                self.add_angle(i)
 
 # def signal_handler(sig, frame):
 #     snake.stop_motor()
