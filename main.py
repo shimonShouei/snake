2c1bc5dbf87153f1e5429fa29eff2f5006e428ed
shimon <shimon.on5@fmail.com>
2021-08-31 16:53:11 +0300

    popen controller fix 1 and middle check fix 1

--- main.py
+++ main.py
@@ -14,6 +14,22 @@ import numpy as np
 import itertools
 import rospy
 from std_msgs.msg import Float32MultiArray, Int32MultiArray
+import roslaunch
+import shlex
+import signal
+import time
+
+
+class GracefulKiller:
+    kill_now = False
+
+    def __init__(self):
+        signal.signal(signal.SIGINT, self.exit_gracefully)
+        signal.signal(signal.SIGTERM, self.exit_gracefully)
+
+    def exit_gracefully(self, *args):
+        self.kill_now = True
+
 
 _max_PWM = 60
 _max_angle = 30
@@ -29,6 +45,7 @@ motor_vel_step_down = -100
 delay = 0.2
 repeat = 8
 possible_actions = list(map(list, itertools.product([motor_vel_step_up, motor_vel_step_down], repeat=repeat)))
+
 
 class SnakeExp:
 
@@ -54,8 +71,8 @@ class SnakeExp:
         self.data.to_csv(self.file_name)
 
     def run_experiment(self):
-        while not rospy.is_shutdown():
-            for i in tqdm.tqdm(range(50)):
+        while not rospy.is_shutdown() and not killer.kill_now:
+            for i in tqdm.tqdm(range(100)):
                 self.middle_check()
                 self.command = random.choice(possible_actions)
                 self.command = Int32MultiArray(data=np.asarray(self.command).astype(int))
@@ -66,6 +83,7 @@ class SnakeExp:
 
             self.data.to_csv(self.file_name, mode='a', header=False)
             self.data = self.data[0:0]
+        self.pub_zero()
 
     def joint_val_update(self, msg):
         # print("msg_data: ", msg.data)
@@ -84,23 +102,37 @@ class SnakeExp:
         self.pub_zero()
         self.final_angles_state = self.current_angles_state
         self.final_strings_state = self.current_strings_state
-        temp_data = self.initial_angles_state+self.initial_strings_state+tuple(self.command.data)+self.final_angles_state+\
+        temp_data = self.initial_angles_state + self.initial_strings_state + tuple(
+            self.command.data) + self.final_angles_state + \
                     self.final_strings_state
         a_series = pd.Series(temp_data, index=self.data.columns)
         self.data = self.data.append(a_series, ignore_index=True)
 
     def stop_motor(self):
-        node_process = Popen(
-            shlex.split('rosrun robot_snake_4 controller_v4_3'), shell=False
-        )
-        time.sleep(8)
+        # node_process = Popen(
+        #     shlex.split('#!/bin/bash rosrun robot_snake_4 controller_v4_3 ')
+        # )
+        # subprocess.Popen(
+        #     '#!/bin/bash rosrun robot_snake_4 controller_v4_3'.split(' '), shell=True
+        # )
+        proc = subprocess.call("~/PycharmProjects/snake/controller.sh", shell=True)
+
+        # node =roslaunch.core.Node("robot_snake_4", "controller_v4_3")
+        # launch = roslaunch.scriptapi.ROSLaunch()
+        # launch.start()
+        #
+        # process = launch.launch(node)
+        # print(process.is_alive())
+        # time.sleep(8)
+        # process.stop()
 
         # stop
-        node_process.terminate()
+        # node_process.terminate()
 
     def pub_zero(self):
         msg = Int32MultiArray(data=np.zeros(_N_string).astype(int))
         self.motor_pwm_pub.publish(msg)
+        rospy.loginfo("Published zero")
 
     def add_tension(self, ind):
         while self.current_strings_state[ind] < min_tension:
@@ -108,15 +140,15 @@ class SnakeExp:
             msg.data[ind] = motor_vel_step_up
             self.command = msg
             self.send_motor_cmd(0.3)
-            logging.info("Adding tension")
+            rospy.loginfo("Adding tension")
 
     def reduce_tension(self, ind):
-        while self.current_strings_state[ind] > max_tension:
+        while self.current_strings_state[ind] > 6:
             msg = Int32MultiArray(data=np.zeros(_N_string).astype(int))
             msg.data[ind] = motor_vel_step_down
             self.command = msg
             self.send_motor_cmd(0.3)
-            logging.info("Reducing tension")
+            rospy.loginfo("Reducing tension")
 
     def add_angle(self, ind):
         msg = Int32MultiArray(data=np.zeros(_N_string).astype(int))
@@ -124,35 +156,37 @@ class SnakeExp:
         msg.data[ind + 1] = motor_vel_step_down
         self.command = msg
         self.send_motor_cmd(0.3)
-        logging.info("Adding angle")
+        rospy.loginfo("Adding angle")
 
     def reduce_angle(self, ind):
         msg = Int32MultiArray(data=np.zeros(_N_string).astype(int))
         msg.data[ind] = motor_vel_step_down
         msg.data[ind + 1] = motor_vel_step_up
-        self.command = msg
-        self.send_motor_cmd(0.3)
-        logging.info("Reducing angle")
+        while self.current_angles_state[ind] > 6:
+            self.command = msg
+            self.send_motor_cmd(0.3)
+            rospy.loginfo("Reducing angle")
+            rospy.loginfo(self.current_angles_state[ind])
 
     def middle_check(self):
         for i in range(len(self.current_strings_state) - 1):
             if self.current_strings_state[i] > max_tension:
-                rospy.logfatal("you reach max tension")
+                rospy.logwarn("you reach max tension")
                 self.reduce_tension(i)
 
             if self.current_strings_state[i] < min_tension:
-                rospy.logfatal("you reach min tension")
+                rospy.logwarn("you reach min tension")
                 self.add_tension(i)
 
         for i in range(len(self.current_angles_state) - 1):
             if self.current_angles_state[i] > max_angl:
-                rospy.logfatal("you reach max angle")
+                rospy.logwarn("you reach max angle")
                 # self.reduce_tension(2*i)
                 # self.add_tension(2*i+1)
                 self.reduce_angle(i)
 
             if self.current_angles_state[i] < min_angl:
-                rospy.logfatal("you reach min angle")
+                rospy.logwarn("you reach min angle")
                 # self.add_tension(2*i)
                 # self.reduce_tension(2*i+1)
                 self.add_angle(i)
@@ -166,9 +200,9 @@ class SnakeExp:
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    logging.getLogger().setLevel(logging.INFO)
     logging.basicConfig(format='%(asctime)s %(message)s')
     rospy.init_node("snake", anonymous=True)
+    killer = GracefulKiller()
     rate = rospy.Rate(100)
     snake = SnakeExp()
     snake.run_experiment()
